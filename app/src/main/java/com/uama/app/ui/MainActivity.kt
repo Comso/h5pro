bd52bac60f3d78de96df314b7d9f72de5874b91b
Cosmo <ruchao.jiang@uama.com.cn>
2019-03-28 20:09:10 +0800

    增加H5utils

--- app/src/main/java/com/uama/app/ui/MainActivity.kt
+++ app/src/main/java/com/uama/app/ui/MainActivity.kt
@@ -33,7 +33,8 @@ class MainActivity : BaseMainActivity() {
         handleEvent()
         commonTab.textSelectColor = Color.RED
         commonTab.textUnselectColor = Color.BLACK
-        goAct(TestListActivity::class)
+        goAct(CommonWebActivity::class)
+        finish()
     }
 
     private fun setBar(){
